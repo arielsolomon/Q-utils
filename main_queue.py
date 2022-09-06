8beee10cb31b22c92352fce3a8288f888727447e
Ariel Solomon <ariel.solomon@gmail.com>
2022-09-06 15:29:14 +0300

    Update main_queue.py

--- main_queue.py
+++ main_queue.py
@@ -5,15 +5,16 @@ import os
 
 def callback(queue_name: str, message: bytes):
     print(f'queue name: {queue_name}, message: {message}')
-img_root = '/Data/Signals/Sat_proj/data/05_04_22dB/images/train'
+img_root = '/home/user1/ariel/Signal/Sat_proj/data/22dB_05_04_22/images/test1'
 img_list = glob.glob(os.path.join(img_root,'*.png'))
 
+# Open single object of each and they connect via callback to the open docker
+
+consumer = QueueConsumer(host='localhost', port=5672, queues_name=['image'], callback=callback)
+publisher = QueuePublisher('localhost', 5672, "image")
+
 for img in img_list:
-    consumer = QueueConsumer(host='localhost', port=5672, queues_name=['image'], callback=callback)
-    publisher = QueuePublisher('localhost', 5672, "image")
     publisher.push_message(img)
-    # publisher.push_message(b'hello world 2')
-    # publisher.push_message(b'hello world 3')
 
 import time
 time.sleep(5)
